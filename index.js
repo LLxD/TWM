d861e28b7fc06c2804cd46f9818ef3e6083965a3
LL <limalucas454@gmail.com>
2022-05-11 14:37:36 -0300

    💄 adding some formatting!

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-console.log("oi")
-
 const pi = 3.14
+
+function showMessage(message) {
+  console.log(message)
+}
+
+const alsoShowsMessage = (message) => console.log(message)
+
+showMessage("Oi")
+alsoShowsMessage("Oi de novo!")
