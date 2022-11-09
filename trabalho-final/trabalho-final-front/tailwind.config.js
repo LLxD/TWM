e21ca657a0168cc591163258a25eee0fa27b356a
LL <limalucas454@gmail.com>
2022-11-09 09:21:02 -0300

    removing unused folder

--- trabalho-final/trabalho-final-front/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    "./src/components/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html",
-    "./src/pages/**/*.{js,jsx,ts,tsx}"
-  ],
-  plugins: [require("daisyui")]
-  // ...
-}
